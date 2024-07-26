799a78fa396fe9f01b70a30e3c4e4641d2fde8a9
roger <fontestz@gmail.com>
2024-07-26 15:58:04 -0300

    Ajuste na configuração do security group da new vpc e multi-az

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ resource "aws_security_group" "rds_sg" {
   vpc_id = var.vpc_id
 
   ingress {
+    description     = "PostgreSQL"
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"

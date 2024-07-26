eb468414e5c2e98a453c7d226a73b6826491a702
roger <fontestz@gmail.com>
2024-07-26 15:08:01 -0300

    Ajuste na configuração do security group

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_db_instance" "default" {
   engine                  = var.db_engine
   instance_class          = var.db_instance_class
   allocated_storage       = var.db_allocated_storage
-  name                    = var.db_name
+  db_name                 = var.db_name
   username                = var.db_username
   password                = var.db_password
   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
